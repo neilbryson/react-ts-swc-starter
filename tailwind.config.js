c3147fe1a45d445d6ca4b3ff25bbe318ed874ff6
Neil Bryson Cargamento <neilbrysonmc@gmail.com>
2022-07-08 16:43:46 +0800

    feat: setup theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
     './public/index.html'
   ],
+  darkMode: 'class',
   theme: {
     screens: {
       sm: '800px',
@@ -13,6 +14,7 @@ module.exports = {
     },
     fontFamily: {
       sans: ['Work Sans', 'sans-serif'],
+      mono: ['ui-monospace', 'monospace'],
     },
   },
   plugins: [],
